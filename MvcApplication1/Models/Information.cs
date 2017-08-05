8aef7a5b2d837bd20ff13c95ad60b6239c941895
unknown <PT812@CSLAB117.CSSERVER.local>
2017-08-05 19:17:59 +0800

    First revision

---
+++ MvcApplication1/Models/Information.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class Information
+    {
+        public List<Accounts> Accounts;
+
+        public Information()
+        {
+            Accounts = new List<Accounts>();
+            Accounts.Add(new Accounts { Id = 4, Name = "Anthony", Age = 6 });
+            Accounts.Add(new Accounts { Id = 4, Name = "asd", Age = 5 });
+            Accounts.Add(new Accounts { Id = 4, Name = "asdf", Age = 4 });
+            Accounts.Add(new Accounts { Id = 4, Name = "123", Age = 1 });
+        }
+    }
+}
